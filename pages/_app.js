e3b8a1179fae543c25ed2e73ed8bb568308d2a43
DeesreX <deesrex@gmail.com>
2023-01-08 04:16:47 -0800

    Added next.js <Head>

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,11 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Rex-topiA</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="A gaming library featuring a collection of JavaScript games created by DeesreX." />
+        <meta name="keywords" content="JavaScript, games, library, Rex-topiA, gaming, DeesreX" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <header className={styles.header}>
 
