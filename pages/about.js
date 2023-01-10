653e6f635e5b8c89c6904244a49c9ebd014e2315
DeesreX <deesrex@gmail.com>
2023-01-10 01:21:11 -0800

    Fixed about issue

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,11 @@
 import About from "../components/about";
 
+export function AboutPage() {
+    return (
+        <div className='aboutcontainer'>
+            <About />
+        </div>
+    );
+}
+
 export default About;
